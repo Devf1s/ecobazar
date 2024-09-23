1e18174f076957b04b20ada0e38a5f9dbe66640c
Devf1s <dragongames829@gmail.com>
2024-09-23 01:21:20 +0100

    update deploy scripts

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero/Hero';
 import Categories from '@/components/Categories/Categories';
 import PopularProducts from '@/components/PopularProducts/PopularProducts';
 import BannerCards from '@/components/BannerCards/BannerCards';
+import HotDeals from '@/components/HotDeals/HotDeals';
 import DiscountBanner from '@/components/DiscountBanner/DiscountBanner';
 import FeaturedProducts from '@/components/FeaturedProducts/FeaturedProducts';
 import News from '@/components/News/News';
@@ -12,7 +13,6 @@ import Posts from '@/components/Posts/Posts';
 import Newsletter from '@/components/Newsletter/Newsletter';
 import Footer from '@/components/Footer/Footer';
 import styles from './Home.module.scss';
-import HotDeals from '@/components/HotDeals/HotDeals';
 
 const Home: FC = () => {
 	return (
